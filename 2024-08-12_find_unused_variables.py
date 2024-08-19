93ceef734c82483b36c9c1b222934719cb7eb9e0
James MIRANDA <james.miranda@ls2n.fr>
2024-08-19 11:09:47 +0200

    Finished unused variables

--- 2024-08-12_find_unused_variables.py
+++ 2024-08-12_find_unused_variables.py
@@ -11,6 +11,7 @@ Examples:
 """
 
 import re
+import numbers
 
 def findLogPattern(variable):
     log = re.search(r"log\(([a-zA-Z])\)", variable)
@@ -23,27 +24,23 @@ def findUnused(variables: list) -> list:
     # for each element of the array, it should check if it is logged and if so, check if it was assigned before and to which variable
     unused_vars = []
     logged_variables = map(findLogPattern, variables)
-    for logged_var in list(logged_variables):
-        if logged_var is None:
-            continue
-        for variable in variables:
-            assign = re.search(r"([A-Za-z]{1}) = ([0-9a-zA-Z])", variable)
-            if assign is not None:
-                # variable name
-                var_name = assign.group(1)
-                var_value = assign.group(2)
-                if var_name == logged_var:
-                    if var_name in unused_vars: unused_vars.remove(var_name)
-                    if var_value in unused_vars: unused_vars.remove(var_value)
-                    continue                
-                # if var_value == logged_var:
-                #     if var_value in unused_vars: unused_vars.remove(var_value)
-                #     continue
-                unused_vars.append(var_name)
+    logged_variables_list = list(logged_variables)
+    
+    for variable in variables:
+        assign = re.search(r"([A-Za-z]{1}) = ([0-9a-zA-Z])", variable)
+        if assign is not None:
+            # variable name
+            var_name = assign.group(1)
+            # variable value
+            var_value = assign.group(2)
+            if var_name not in logged_variables_list:
+                if var_name not in unused_vars: unused_vars.append(var_name)
+            else:           
+                if not isinstance(var_value, numbers.Number) and var_value in unused_vars:
+                    unused_vars.remove(var_value)
 
     print(unused_vars)
     return unused_vars
-
 
 
 if __name__ == "__main__":
@@ -52,3 +49,4 @@ if __name__ == "__main__":
     assert findUnused(["m = 3", "n = m", "o = n", "log(m)"]) == ['n', 'o']
     assert findUnused(["x = 5", "y = x", "z = 10", "log(z)", "log(y)"]) == []
     assert findUnused(["a = 1", "b = 2", "c = 3", "d = c", "log(d)", "log(a)"]) == ['b']
+    print("All tests passed")
